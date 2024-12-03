28dbc63148f7d2481164b5721c10a7bc2b2658b5
Henry Erondu <119769018+henchmotion@users.noreply.github.com>
2024-12-02 16:43:32 -1200

    Controllers Correction

--- obj/Debug/net8.0/HEWalks.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/HEWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HEWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b30013fa9d6a7ab3799d9ceb395b9e6eea1cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HEWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HEWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
