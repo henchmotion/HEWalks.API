8176aada1e1da222fb152ddd306b51c49ab17c48
Henry Erondu <119769018+henchmotion@users.noreply.github.com>
2024-12-13 02:27:29 -1200

    data seeding step 1

--- Data/HEWalksDbContext.cs
+++ Data/HEWalksDbContext.cs
@@ -15,6 +15,37 @@ namespace HEWalks.API.Data
         public DbSet<Region> Regions { get; set; }
 
         public DbSet<Walk> Walks { get; set; }
-    }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+            // Seed data for Difficulites
+            // Easy, Medium, Hard
+
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty()
+                {
+                    Id = Guid.Parse("39de6daf-ba70-451d-8f8a-1457c27c2535"),
+                    Name = "Easy"
+                },
+
+                new Difficulty()
+                {
+                    Id = Guid.Parse("3a5f8381-dc91-4c42-81b6-b141ab13fe86"),
+                    Name = "Medium"
+                },
+
+                new Difficulty()
+                {
+                    Id = Guid.Parse("a05591ce-005c-43bc-95e8-82519ccc057d"),
+                    Name = "Hard"
+                }
+            }; 
+
+
+		}
+	}
 }
 
